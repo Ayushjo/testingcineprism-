faac4d6aea6cd6812fc654a8a0336358dffb2b90
AyushJo <ayushs24101@iiitnr.edu.in>
2025-08-05 18:59:38 +0530

    changed the backend url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3500",
+        target: "https://testingcineprismbackend-production.up.railway.app/",
         changeOrigin: true,
         secure: false,
       },
