41828a260668f5af6f7c10581936080ba1c17f77
AyushJo <ayushs24101@iiitnr.edu.in>
2025-08-28 11:39:12 +0530

    fix: updated landing page

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -17,6 +17,7 @@ const navigationItems = [
   { icon: ImageUp, label: "Upload Poster", id: "upload-poster" },
   { icon: ImageUp, label: "Upload Reviews Poster", id: "upload-review-poster" },
   { icon: Images, label: "Upload Gallery", id: "upload-gallery" },
+  {icone:FilePlus2,label:"Edit Post",id:"edit-post"}
 ];
 
 export default function AdminLayout() {
